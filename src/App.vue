ef9f73006f58f011551d44420791031291a0594f
Iván <ilm13371@gmail.com>
2022-05-10 21:35:33 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TimeWatch from './components/TimeWatch.vue'
+</script>
+
+<template>
+<div class="container">
+ <TimeWatch />
+
+</div>
+</template>
+
+<style>
+@import './assets/base.css';
+
+.container {
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/components/TimeWatch.vue
@@ -0,0 +1,53 @@
+<script>
+
+  export default {
+    name: "TimeWatch",
+    data(){
+      return {
+        hours: '--',
+        minutes: '--',
+        seconds: '--'
+      }
+    },
+    mounted() {
+      setInterval(()=> this.updateClock(), 1000)
+    },
+    methods: {
+      addLeadingZeros(num, totalLength) {
+        return String(num).padStart(totalLength, '0');
+      },
+      updateClock() {
+        const date = new Date();
+        this.hours = this.addLeadingZeros(date.getHours(), 2);
+        this.minutes = this.addLeadingZeros(date.getMinutes(), 2);
+        this.seconds = this.addLeadingZeros(date.getSeconds(), 2);
+
+      }
+    },
+  }
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="watch">
+      <p>{{hours}} : {{minutes}} : {{seconds}}</p>
+    </div>
+  </div>
+</template>
+
+
+<style>
+.container {
+  display: flex;
+  color: white;
+
+}
+.watch{
+  font-family: 'Orbitron', monospace;
+  font-size: 80px;
+  display: inline-block;
+  text-shadow: 0px 0px 8px greenyellow;
+}
+
+</style>
